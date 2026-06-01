bb734c04071a8d6674090bbc8515a5f0a67f57f6
ebuka0002 <ebukaukaji@gmail.com>
2026-06-01 16:03:02 +0100

    fixed deployment

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
   build: {
     outDir: 'dist',
     sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          motion: ['framer-motion'],
-        },
-      },
-    },
-  },
-  server: {
-    port: 3000,
-    open: true,
   },
 })
